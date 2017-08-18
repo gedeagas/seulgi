517eede13c86b4f01e2b3b217ec7d301899b35bb
Agastya Darma <gedeagas22@gmail.com>
2017-08-19 05:13:55 +0700

    Bug Fixing on verifySignature

--- app/service/verifyRequestSignature.js
+++ app/service/verifyRequestSignature.js
@@ -12,6 +12,35 @@
  *
  */
 const crypto = require('crypto');
+const config = require('config'),
+
+
+/*
+ * Open config/default.json and set your config values before running this code. 
+ * You can also set them using environment variables.
+ *
+ */
+
+// App Secret can be retrieved from the App Dashboard
+const APP_SECRET = (process.env.MESSENGER_APP_SECRET) ? 
+process.env.MESSENGER_APP_SECRET :
+config.get('appSecret');
+
+// Arbitrary value used to validate a webhook
+const VALIDATION_TOKEN = (process.env.MESSENGER_VALIDATION_TOKEN) ?
+(process.env.MESSENGER_VALIDATION_TOKEN) :
+config.get('validationToken');
+
+// Generate a page access token for your page from the App Dashboard
+const PAGE_ACCESS_TOKEN = (process.env.MESSENGER_PAGE_ACCESS_TOKEN) ?
+(process.env.MESSENGER_PAGE_ACCESS_TOKEN) :
+config.get('pageAccessToken');
+
+// making sure that everything has been properly configured
+if (!(APP_SECRET && VALIDATION_TOKEN && PAGE_ACCESS_TOKEN)) {
+    console.error("Missing config values");
+    process.exit(1);
+}
 
 export default function verifyRequestSignature(req, res, buf) {
     var signature = req.headers["x-hub-signature"];
