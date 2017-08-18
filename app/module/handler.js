7beef7e8cb114aff6fb061b0e5fa36941cfb0260
Agastya Darma <gedeagas22@gmail.com>
2017-08-19 05:36:40 +0700

    Language Detection System

--- app/module/handler.js
+++ app/module/handler.js
@@ -1,4 +1,5 @@
 const config = require('config');
+const franc = require('franc');
 
 // Generate a page access token for your page from the App Dashboard
 const PAGE_ACCESS_TOKEN = (process.env.MESSENGER_PAGE_ACCESS_TOKEN) ?
@@ -13,11 +14,27 @@ export const handler = (data) => {
     let pageID = data.recipient.id;
     let timeOfMessage = data.timestamp;
     let message = data.message;
+    let senderName = "";
 
+    let lang = franc(message, {blacklist: ['src', 'zlm']});
+    
     console.log("[receivedMessage] user (%d) page (%d) timestamp (%d) and message (%s)", 
     senderID, pageID, timeOfMessage, JSON.stringify(message));
+    
+    
 
-    messenger.sendTextMessage(senderID, 'Diterima Cuy');
+    messenger.getProfile(senderID, function (err, body) {
+        if(err) return console.error(err)
+        console.log(body)
+    });
+
+    if(lang == "eng") {
+        messenger.sendTextMessage(senderID, 'English');
+        
+    } else {
+        messenger.sendTextMessage(senderID, 'Maaf , bahasa indonesia ku belum bagus');
+        
+    }
 }
 
 export default handler;
