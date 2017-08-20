086e1d51431478e431a3d1602140df8db9a7fffa
Agastya Darma <gedeagas22@gmail.com>
2017-08-21 00:30:08 +0800

    Kumpul.co Order Pass

---
+++ app/intentresolver/order.js
@@ -0,0 +1,50 @@
+export const orderFulfilment = (profile, data, response) => {
+  function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+  }
+
+  const orderNumber = getRandomArbitrary(1200000, 2100000);
+  const todayTimestamp = new Date();
+  let pricing = 0;
+  const orderTimespan = response.result.contexts[1].parameters.timespan;
+  if (orderTimespan === 'daily') {
+    pricing = 200000;
+  } else {
+    pricing = 2600000;
+  }
+
+  const resep = {
+    recipient_name: profile.first_name,
+    order_number: orderNumber,
+    currency: 'IDR',
+    payment_method: 'Website',
+    order_url: 'https://www.kumpul.co/membership/join/',
+    timestamp: todayTimestamp,
+    address: {
+      street_1: 'Rumah Sanur',
+      street_2: 'Jl. Danau Poso 51A Br. Semawang',
+      city: 'Sanur',
+      postal_code: '80228',
+      state: 'Bali',
+      country: 'Indonesia',
+    },
+    summary: {
+      subtotal: pricing,
+      total_tax: 0,
+      total_cost: pricing,
+    },
+    elements: [
+      {
+        title: `${orderTimespan} Coworking Membership`,
+        subtitle: 'Welcome to Kumpul Coworking Space',
+        quantity: 1,
+        price: pricing,
+        currency: 'IDR',
+        image_url: 'https://www.kumpul.co/images/beanbags.jpg',
+      },
+    ],
+  };
+  return resep;
+};
+
+export default orderFulfilment;
--- app/module/engineEnglish.js
+++ app/module/engineEnglish.js
@@ -1,4 +1,5 @@
 import { PAGE_ACCESS_TOKEN } from './token';
+import orderFulfilment from '../intentresolver/order';
 
 const apiai = require('apiai');
 const FBMessenger = require('fb-messenger');
@@ -26,63 +27,22 @@ export default function engineEnglish(data) {
 
   request.on('response', (response) => {
     if (isDefined(response.result) && isDefined(response.result.fulfillment)) {
+      if (response.result.action === 'transactionorder.transactionorder-custom.transactionorder-whichone-yes') {
+        messenger.getProfile(senderID, (err, body) => {
+          const userProfile = body;
+          const speech = response.result.fulfillment.speech;
+
+          messenger.sendTextMessage(senderID, `Okay ${userProfile.first_name}, ${speech}`);
+          messenger.sendReceiptMessage(senderID, orderFulfilment(userProfile, data, response));
+        });
+      }
       if (response.result.metadata.intentName === 'smalltalk.agent.beautiful') {
         messenger.getProfile(senderID, (err, body) => {
           const userProfile = body;
           const speech = response.result.fulfillment.speech;
-          const resep = {
-            recipient_name: 'Stephane Crozatier',
-            order_number: '12345678902',
-            currency: 'USD',
-            payment_method: 'Visa 2345',
-            order_url: 'http://petersapparel.parseapp.com/order?order_id=123456',
-            timestamp: '1428444852',
-            address: {
-              street_1: '1 Hacker Way',
-              street_2: '',
-              city: 'Menlo Park',
-              postal_code: '94025',
-              state: 'CA',
-              country: 'US',
-            },
-            summary: {
-              subtotal: 75.00,
-              shipping_cost: 4.95,
-              total_tax: 6.19,
-              total_cost: 56.14,
-            },
-            adjustments: [
-              {
-                name: 'New Customer Discount',
-                amount: 20,
-              },
-              {
-                name: '$10 Off Coupon',
-                amount: 10,
-              },
-            ],
-            elements: [
-              {
-                title: 'Classic White T-Shirt',
-                subtitle: '100% Soft and Luxurious Cotton',
-                quantity: 2,
-                price: 50,
-                currency: 'USD',
-                image_url: 'http://petersapparel.parseapp.com/img/whiteshirt.png',
-              },
-              {
-                title: 'Classic Gray T-Shirt',
-                subtitle: '100% Soft and Luxurious Cotton',
-                quantity: 1,
-                price: 25,
-                currency: 'USD',
-                image_url: 'http://petersapparel.parseapp.com/img/grayshirt.png',
-              },
-            ],
-          };
+
           messenger.sendTextMessage(senderID, `${speech} ${userProfile.first_name}`);
           messenger.sendImageMessage(senderID, 'https://68.media.tumblr.com/69a5c69c36489d90d9838850f5668af6/tumblr_ou0embggVR1ssnf1qo6_250.gif');
-          messenger.sendReceiptMessage(senderID, resep);
         });
       } else {
         messenger.sendTextMessage(senderID, response.result.fulfillment.speech);
