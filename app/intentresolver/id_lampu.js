6e40e1a57da0a4be1b2b449df3330f0bb172f28c
Agastya Darma <gedeagas22@gmail.com>
2017-08-23 23:54:08 +0800

    Entities Bug Fix 2

--- app/intentresolver/id_lampu.js
+++ app/intentresolver/id_lampu.js
@@ -5,7 +5,7 @@ const client = mqtt.connect('mqtt://45.32.115.11');
 
 export const idLampuFulfilment = (profile, data, response) => {
   let action = '';
-  if (response.entities.on_off.value === 'on') {
+  if (response.entities.on_off[0].value === 'on') {
     client.publish('lampu', 'on');
     action = 'dinyalakan';
   } else {
