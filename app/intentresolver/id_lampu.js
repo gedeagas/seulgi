3ae7b9777bcfa28b11068393ca0d70a4829cb058
Agastya Darma <gedeagas22@gmail.com>
2017-08-23 23:58:22 +0800

    Entities Bug Fix 3

--- app/intentresolver/id_lampu.js
+++ app/intentresolver/id_lampu.js
@@ -7,15 +7,14 @@ export const idLampuFulfilment = (profile, data, response) => {
   let action = '';
   if (response.entities.on_off[0].value === 'on') {
     client.publish('lampu', 'on');
-    action = 'dinyalakan';
+    action = 'nyalakan';
   } else {
     client.publish('lampu', 'off');
-    action = 'dimatikan';
+    action = 'matikan';
   }
   const device = response.entities.iot_things[0].value;
   const place = response.entities.iot_place[0].value;
   const returnSpeech = `${profile.first_name}, ${device} ${place} sudah aku ${action}`;
   return returnSpeech;
 };
-
 export default idLampuFulfilment;
